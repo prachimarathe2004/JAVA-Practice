ab95de4bcc2857f33a0942bc91a38df2b0b87089
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-01 16:42:22 +0530

    Add Demo3.java with a function to add two numbers

---
+++ Functions/Demo3.java
@@ -0,0 +1,14 @@
+//Function WITH return value
+
+
+public class Demo3 {
+
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println("Sum = " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
