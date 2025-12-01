2b040d6b5259bf952b55597e8218402dffab0e2f
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-01 11:26:23 +0530

    Add Demo1 class with greeting method

---
+++ Functions/Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1 {
+
+    public static void main(String[] args) {
+        greet();   // calling the method
+    }
+
+    public static void greet() {
+        System.out.println("Hello, welcome to Java!");
+    }
+}
