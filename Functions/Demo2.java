f7e10a13f52f7eaf64e7122a91bf4ee2a3dc127a
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-01 11:31:17 +0530

    Add Demo2.java with greetUser function

---
+++ Functions/Demo2.java
@@ -0,0 +1,13 @@
+//PROGRAM 2: Function WITH parameters
+
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        greetUser("Prachi");
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello " + name + "!");
+    }
+}
