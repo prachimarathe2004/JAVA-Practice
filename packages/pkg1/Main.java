0a88409661b04cb97debcd1f2e1e9b775f65986a
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-02 11:21:43 +0530

    Implement main class to use Calculator methods

---
+++ packages/pkg1/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        System.out.println("Add = " + calc.add(10, 20));
+        System.out.println("Multiply = " + calc.multiply(3, 5));
+    }
+}
