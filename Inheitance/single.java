7b5517eb3fdf2db0b84d7c8b9b397d1627a7e88e
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-16 11:22:14 +0530

    Implement single inheritance with Vehicle and Car classes

---
+++ Inheitance/single.java
@@ -0,0 +1,23 @@
+//Single Inheritance
+
+
+//Super class
+class Vehicle {
+    Vehicle() {
+        System.out.println("This is a Vehicle");
+    }
+}
+
+// Subclass 
+class Car extends Vehicle {
+    Car() {
+        System.out.println("This Vehicle is Car");
+    }
+}
+
+public class single {
+    public static void main(String[] args) {
+        // Creating object of subclass invokes base class constructor
+        Car obj = new Car();
+    }
+}
