1025f2564ea72bed4ddc488bb6019e677cf52a8b
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-16 11:20:53 +0530

    Implement multilevel inheritance example

---
+++ Inheitance/multilevel.java
@@ -0,0 +1,23 @@
+//Multilevel Inheritance
+
+
+class Vehicle {
+    Vehicle() {
+        System.out.println("This is a Vehicle");
+    }
+}
+class FourWheeler extends Vehicle {
+    FourWheeler() {
+        System.out.println("4 Wheeler Vehicles");
+    }
+}
+class Car extends FourWheeler {
+    Car() {
+        System.out.println("This 4 Wheeler Vehicle is a Car");
+    }
+}
+public class multilevel {
+    public static void main(String[] args) {
+        Car obj = new Car(); // Triggers all constructors in order
+    }
+}
